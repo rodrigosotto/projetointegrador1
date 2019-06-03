152813cf24033289ab0b7663a74a6c0e86f9189d
Jeffersontads <jeffersontads@hotmail.com>
2019-06-02 21:14:24 -0300

    corrigindo erros de paginas e layou, projeto atualizado.

--- src/providers/users-providers/users-providers.ts
+++ src/providers/users-providers/users-providers.ts
@@ -63,7 +63,7 @@ export class UsersProvider {
 //metodos
   get(id: number) {
     return new Promise((resolve, reject) => {
-      let url = this.API_URL + 'users' + id;
+      let url = this.API_URL + 'usuariosz/' + id;
 
       this.http.get(url)
         .subscribe((result: any) => {
@@ -77,7 +77,7 @@ export class UsersProvider {
 //metodos
   insert(user: any) {
     return new Promise((resolve, reject) => {
-      let url = this.API_URL + 'users/';
+      let url = this.API_URL + 'usuarioz/';
 
       this.http.post(url, user)
         .subscribe((result: any) => {
@@ -91,7 +91,7 @@ export class UsersProvider {
 
   update(user: any) {
     return new Promise((resolve, reject) => {
-      let url = this.API_URL + 'users/' + user.id;
+      let url = this.API_URL + 'usuarioz/' + user.id;
       let data = {
         "nome": user.nome,
         "sobrenome": user.sobrenome
