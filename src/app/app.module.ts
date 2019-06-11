d792983d4cdaed8f7e14fe7e9e420a6dc7eade63
vfporto <2643007+vfporto@users.noreply.github.com>
2019-06-11 15:28:33 -0300

    correção para enviar automaticamente o token a cada requisição

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ export function jwtOptionsFactory(storage) {
     tokenGetter: () => {
       return storage.get('access_token');
     },
-    whitelistedDomains: ['localhost:8000/api']
+    whitelistedDomains: ['localhost:8000']
   }
 }
 
