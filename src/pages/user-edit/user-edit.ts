3e2c3eae36e3214d843f16e25b4ac40033584fe0
Jeffersontads <jeffersontads@hotmail.com>
2019-06-15 16:48:07 -0300

    Atualizando o repositorio com app atualizado

--- src/pages/user-edit/user-edit.ts
+++ src/pages/user-edit/user-edit.ts
@@ -2,6 +2,7 @@ import { UsersProvider } from './../../providers/users-providers/users-providers
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { EmailValidator } from '@angular/forms';
+import { AreasProvidersProvider } from '../../providers/areas-providers/areas-providers';
 
 @IonicPage()
 @Component({
@@ -11,25 +12,57 @@ import { EmailValidator } from '@angular/forms';
 export class UserEditPage {
 
   model: User;
+  listaAreas: any[];
+
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private toast: ToastController,
-    private userProvider: UsersProvider ) {
+    private userProvider: UsersProvider, 
+    private areaProvider:AreasProvidersProvider) {
+
     if (this.navParams.data.user) {
       this.model = this.navParams.data.user;
     } else {
       this.model = new User();
     }
+    this.getAllAreas();
   }
+
+  /*metodo para buscar todas as areas no editar*/
+
+  getAllAreas() {
+    this.areaProvider.getAllAreas()
+      .then((result: any) => {
+        this.listaAreas = result;
+        console.log("areas: "+this.listaAreas);
+         
+
+        /*for (var i = 0; i < result.data.length; i++) {
+          var user = result.data[i];
+        }
+
+        if (this.infiniteScroll) {
+          this.infiniteScroll.complete();
+          if (this.users.length == result.total) {
+            this.infiniteScroll.enable(false);
+          }
+        }*/
+      })
+      .catch((error: any) => {
+        this.toast.create({ message: 'Erro ao listar Areas. Erro: ' + error,
+        position: 'botton', duration: 3000 }).present();
+      });
+  }
+
 
   save() {
     this.saveUser()
       .then(() => {
         this.toast.create({ message: 'Usuário salvo com sucesso.',
         position: 'botton', duration: 3000 }).present();
-        //this.navCtrl.pop();
+        this.navCtrl.pop();
       })
       .catch((error) => {
         this.toast.create({ message: 'Erro ao salvar o usuário. Erro: ' + error.error.error, position: 'botton',
@@ -49,7 +82,6 @@ export class UserEditPage {
 
 export class User {
   id: number;
-  areaDeTrabalho: string;
   login: string;
   nome: string;
   email:EmailValidator;
@@ -61,5 +93,6 @@ export class User {
     nome: string;
   };*/
   tipo_usuario_id: number;
+  area_id: number;
 
 }
